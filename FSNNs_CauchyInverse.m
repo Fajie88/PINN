f26a1cf8bdab2988c2ee28bf056e9e63bd944e4a
Fajie Wang <117610841+Fajie88@users.noreply.github.com>
2025-09-03 16:23:13 +0800

    FSNNs_CauchyInverse.m

--- FSNNs_CauchyInverse.m
+++ FSNNs_CauchyInverse.m
@@ -19,9 +19,9 @@
 %  of or in connection with the code or the use or other dealings in the code.
 %  --------------------------------------------------------------------------
 %  References：
-%  https://ww2.mathworks.cn/help/deeplearning/ug/solve-partial-differential-equations-with-lbfgs-method-and-deep-learning.html
-%  F. Wang, X. Li, H. Liu, L. Qiu, X. Yue. An adaptive method of fundamental solutions using physics-informed neural networks. 
-%    Eng Anal Bound Elem. 2025, 178:106295. https://doi.org/10.1016/j.enganabound.2025.106295
+%  [1] https://ww2.mathworks.cn/help/deeplearning/ug/solve-partial-differential-equations-with-lbfgs-method-and-deep-learning.html
+%  [2] F. Wang, X. Li, H. Liu, L. Qiu, X. Yue. An adaptive method of fundamental solutions using physics-informed neural networks. 
+%      Eng Anal Bound Elem. 2025, 178:106295. https://doi.org/10.1016/j.enganabound.2025.106295
 %  --------------------------------------------------------------------------
 %  Qingdao University 
 %  College of Mechanical and Electrical Engineering, Qingdao University
@@ -393,6 +393,7 @@ U_S = extractdata(U_S);
 E_S = u_exact(S(:,1),S(:,2));
 E_globalS = (sum(abs(U_S - E_S).^2)./(sum(E_S.^2))).^0.5;
 end
+
 
 
 
